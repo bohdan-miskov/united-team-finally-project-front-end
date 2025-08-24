9298057017ba0c79aff088a4379fa39fe5bb03a6
VickyUA <vickyy379@gmail.com>
2025-08-24 22:03:47 +0300

    NotFound-component-added

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,3 +1,28 @@
+import { useMediaQuery } from 'react-responsive';
+import css from './NotFound.module.css';
+import Image from '../../assets/img/not-found/plate-mob.webp';
+import ImageTablet from '../../assets/img/not-found/plate-tab.webp';
+
 export default function NotFound() {
-  return;
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const isTabletOrDesktop = useMediaQuery({ minWidth: 768 });
+
+  return (
+    <section className={css.desktopSection}>
+      <div className={`container ${css.wrapper}`}>
+        {isMobile && <img className={css.img} src={Image} alt="omelette" />}
+        {isTabletOrDesktop && (
+          <img className={css.img} src={ImageTablet} alt="omelette" />
+        )}
+        <h1 className={css.h1}>404</h1>
+        <h3 className={css.h3}>Recipe not found</h3>
+        <button type="button" className={`brown-btn ${css.button}`}>
+          <svg className={css.icon} width={24} height={24}>
+            <use href="/icons.svg#icon-left-short-arrow"></use>
+          </svg>
+          Back To Home
+        </button>
+      </div>
+    </section>
+  );
 }
--- src/pages/RecipeViewPage/RecipeViewPage.jsx
+++ src/pages/RecipeViewPage/RecipeViewPage.jsx
@@ -1,5 +1,11 @@
-import RecipeDetails from "../../components/RecipeDetails/RecipeDetails";
+import RecipeDetails from '../../components/RecipeDetails/RecipeDetails';
+import NotFound from '../../components/NotFound/NotFound';
 
 export default function RecipeViewPage() {
-  return <RecipeDetails />;
+  return (
+    <>
+      <RecipeDetails />
+      <NotFound />
+    </>
+  );
 }
