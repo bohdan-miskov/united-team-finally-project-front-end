e370b2c1cda32fcc8d281ff981f01f8e9dba4dcd
Bohdan <bohdanmiskov07@gmail.com>
2025-08-24 13:01:21 +0300

    Add exception path for api interceptors

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,11 +1,11 @@
-import axios from "axios";
-import { refreshUser } from "../redux/auth/operations";
+import axios from 'axios';
+import { refreshUser } from '../redux/auth/operations';
 
 const api = axios.create({
-  baseURL: "https://united-team-finally-project-backend.onrender.com",
+  baseURL: 'https://united-team-finally-project-backend.onrender.com',
 });
 
-export const setAuthHeader = (token) => {
+export const setAuthHeader = token => {
   api.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
@@ -13,29 +13,32 @@ export const clearAuthHeader = () => {
   delete api.defaults.headers.common.Authorization;
 };
 
-// Змінна для передачі store
 let store;
-export const injectStore = (_store) => {
+export const injectStore = _store => {
   store = _store;
 };
 
-// Інтерсептор з викликом refreshUser через dispatch
 api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
+  response => response,
+  async error => {
     const originalRequest = error.config;
 
-    if (error.response?.status === 401 && !originalRequest._retry) {
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry &&
+      !['/auth/login', '/auth/register', '/auth/refresh'].includes(
+        originalRequest.url
+      )
+    ) {
       originalRequest._retry = true;
 
-      if (!store) return Promise.reject(error); // якщо store не підключений
+      if (!store) return Promise.reject(error);
 
       try {
-        // Викликаємо refreshUser thunk через dispatch
         const resultAction = await store.dispatch(refreshUser());
 
         if (refreshUser.fulfilled.match(resultAction)) {
-          return api(originalRequest); // повторюємо запит
+          return api(originalRequest);
         } else {
           return Promise.reject(error);
         }
