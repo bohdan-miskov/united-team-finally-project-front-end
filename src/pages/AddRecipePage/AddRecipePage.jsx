77789170428233f34ed100ba182b03ba0ce4d95d
Mary <maryhoglan78@gmail.com>
2025-08-25 14:54:47 +0300

    add code AddRecipte(page,form), css(only mob version), svg icon-photo

--- src/pages/AddRecipePage/AddRecipePage.jsx
+++ src/pages/AddRecipePage/AddRecipePage.jsx
@@ -1,3 +1,12 @@
+import css from "./AddRecipePage.module.css"
+import AddRecipeForm from "../../components/AddRecipeForm/AddRecipeForm"
+
 export default function AddRecipePage() {
-  return <p>AddRecipePage</p>;
+  return(
+  <div className={css.page}>
+    <h2 className={css.title}>Add Recipe</h2>
+    <AddRecipeForm />
+  </div>
+  
+  );
 }
